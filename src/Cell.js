e9d4d926ebc6800b4b49453878dafac0b930dbc4
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-13 18:45:26 +0100

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class Cell extends Component {
+  state = { color: this.props.value };
+
+  constructor(props) {
+    super(props);
+    this.state.color = this.props.value;
+  }
+
+  handleClickEvent = () => {
+    this.setState({ color: "#333" });
+  };
+
+  render() {
+    return (
+      <div
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClickEvent}
+        className="cell"
+      >
+        .
+      </div>
+    );
+  }
+}
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Cell from "./Cell";
+import { defaultPattern } from "./data";
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
-  genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
-  
-  render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
+  genRow = (
+    vals // vals is an array of 10 strings
+  ) =>
+    vals.map(val => (
+      <div className="cell">
+        <Cell value={val} />
       </div>
-    )
+    ));
+
+  genMatrix = () =>
+    this.props.values.map(rowVals => (
+      <div className="row">{this.genRow(rowVals)}</div>
+    ));
+
+  render() {
+    return <div id="matrix">{this.genMatrix()}</div>;
   }
-  
 }
+
+Matrix.defaultProps = {
+  values: defaultPattern
+};
